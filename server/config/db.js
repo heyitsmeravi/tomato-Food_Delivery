68f840a3883a38559e298aa44401f04e3cfde981
heyitsmeravi <ravijnv2428@gmail.com>
2026-09-20 00:18:09 +0530

    changes in security

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://heyitsmeravi01_db_user:5CZZNCzKKYVYmPfO@cluster0.gw7tkdl.mongodb.net/Tomato_Food-Delivery-System')
+    await mongoose.connect(process.env.MONGO_URI)
     .then(()=>{ console.log("Connected to MongoDB"); })
     .catch((err)=>{ console.log(err); });
 }
